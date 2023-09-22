196abdd50239cb7e2738879b22b2b5ccdf2f7819
Divya <divyareddy0205@gmail.com>
2023-09-22 15:14:09 -0700

    Create-react-app and tailwind configured

---
+++ README.md
@@ -0,0 +1,8 @@
+# Netflix GPT
+
+Create React App
+Configured TailwindCSS
+Header
+Routing of App
+Login Form
+Sign up Form
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+          <h2 className="font-bold text-2xl text-slate-600">Netflix GPT</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
