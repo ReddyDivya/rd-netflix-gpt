24289ab93f419c71ad633eb3b2f06f199f6bb3c9
Divya <divyareddy0205@gmail.com>
2023-09-22 16:32:51 -0700

    login-component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div>
       <header>
-          <h2 className="font-bold text-2xl text-slate-600">Netflix GPT</h2>
           <Body/>
       </header>
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import Header from "./Header";
+import React, {useState} from 'react'
+// import Header from "./Header";
+import { BG_URL } from "../utils/constants";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
   return (
     <div>
-        <div>Login</div>
+        <div className="absolute">
+          <img className="" src={BG_URL} alt="logo"/>
+        </div>
+        <form className="w-full absolute md:w-3/12 p-12 bg-black my-30 mt-10 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+            <h1 className="font-bold text-3xl py-2">
+              {isSignInForm ? "Sign In" : "Sign Up"}
+            </h1>
+
+            <input type="text" placeholder="Full Name" className="p-3 my-4 w-full bg-gray-700"/>
+            <input type="text" placeholder="Email address" className="p-3 my-4 w-full bg-gray-700"/>
+            <input type="text" placeholder="Password" className="p-3 my-4 w-full bg-gray-700"/>
+
+            <p className="text-red-500 font-bold text-lg py-2">Error Message</p>
+            <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+              {isSignInForm ? "Sign In" : "Sign Up"}
+            </button>
+            <p className="py-2 cursor-pointer">{isSignInForm ? "New to Netflix? Sign up Now" : "Already registered? Sign In Now."}</p>
+        </form>
     </div>
   )
 }
