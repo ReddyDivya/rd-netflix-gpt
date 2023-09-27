e773c5fa873024103c88dfa4b27957661b070cff
Divya <divyareddy0205@gmail.com>
2023-09-27 13:54:25 -0700

    BugFix: if the user is not logged in Redirect /browse to Login Page and vice-versa

--- README.md
+++ README.md
@@ -6,3 +6,14 @@
 - Routing of App
 - Login Form
 - Sign up Form
+- Form Validation
+- useRef Hook
+- Firebase Setup
+- Deploying our app to production
+- Create SignUp User Account
+- Implement Sign In user Api
+- Created Redux Store with userSlice
+- Implemented Sign out
+- Update Profile
+- BugFix: Sign up user displayName and profile picture update
+- BugFix: if the user is not logged in Redirect /browse to Login Page and vice-versa
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {LOGO} from "../utils/constants";
 import { useSelector } from 'react-redux';
 import { signOut } from "firebase/auth";
 import {auth} from "../utils/firebase";
 import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from 'react-redux';
+import {addUser, removeUser} from "../utils/userSlice";
 
 const Header = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   //fetching user details from the redux store using the useSelector
   const user = useSelector((store) => store.user);
 
+  //signout
   const handleSignout = () => {
     signOut(auth).then(() => {
       // Sign-out successful.
-      navigate("/");
+      //automatically triggers onAuthStateChanged
     }).catch((error) => {
       navigate("/error");
     });
-  }
+  }//handleSignout
+
+  //pushing the userInfo while Sign up, Sign in to the redux store.
+  useEffect(() => {
+    //onAuthStateChanged is called during Sign up, sign in, sign out.
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) 
+        {
+            //Sign up, sign in
+          const {uid, displayName, email, photoURL} = user;
+          dispatch(addUser({email:email, displayName:displayName, uid:uid, photoURL : photoURL}))
+          navigate("/browse");
+        } else {
+            //sign out
+            dispatch(removeUser());
+            navigate("/");
+        }
+      });
+
+      //cleaning. It's called when the onAuthStateChanged unmounts
+      return() => unsubscribe();
+}, []);
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
@@ -26,7 +52,7 @@ const Header = () => {
 
         {
           user && <div className="flex m-2">
-          <img className="w-14 h-14 mx-4" src={user.photoURL}/>
+          <img className="w-14 h-14 mx-4" alt="user-photo" src={user.photoURL}/>
           <button onClick={handleSignout} className="bg-red-500 font-bold w-20 h-10 mt-2 rounded-lg text-white">Signout</button>
         </div>
         }
