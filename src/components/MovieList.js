960f43aab35804fbc075eea99f948ce55547be86
Divya <divyareddy0205@gmail.com>
2023-10-06 17:45:04 -0700

    BugFix: scrollbar, width

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,13 +7,13 @@ const MovieList = ({title, movies}) => {
     <div className="px-6">
         <h1 className="md:text-xl font-bold mt-2 pt-4 pb-2 text-white">{title}</h1>
         <div className="flex overflow-x-scroll no-scrollbar">
-            <div className="absolute z-1 -ml-12 w-10 h-[170px] bg-gradient-to-r from-gray-800 to-transparent py-16 px-2"><i className="text-4xl text-white font-bold"><MdOutlineArrowBackIosNew/></i></div>
-              <div className="flex">
-                  {
-                      movies?.map((movie) => <MovieCard key={movie.id} posterPath={movie.poster_path}/>)
-                  }
-              </div>
-            {/* <div className="absolute z-2 w-10 h-[170px] bg-gradient-to-r from-gray-800 to-transparent py-16 px-2"><i className="text-4xl text-white font-bold"><MdOutlineArrowForwardIos/></i></div> */}
+            <div className="flex">
+                <div className="absolute z-1 -ml-1 w-10 h-[170px] bg-gradient-to-r from-gray-800 to-transparent py-16 px-2"><i className="text-4xl text-white font-bold"><MdOutlineArrowBackIosNew/></i></div>
+                {
+                    movies?.map((movie) => <MovieCard key={movie.id} posterPath={movie.poster_path}/>)
+                }
+                <div className="absolute z-2 ml-[93%] mr-10 w-10 h-[170px] bg-gradient-to-r from-gray-800 to-transparent py-16 px-2"><i className="text-4xl text-white font-bold"><MdOutlineArrowForwardIos/></i></div>
+            </div>
         </div>
     </div>
   )
