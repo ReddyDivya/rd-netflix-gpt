1e1ad0e18531c1248c1b65967cc6365ab3a5ffeb
Divya <divyareddy0205@gmail.com>
2023-12-02 19:42:06 -0800

    gpt-search-bar

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,11 +1,8 @@
 import React, { useRef } from 'react'
 import lang from "../utils/languageConstants";
 import { useDispatch, useSelector } from 'react-redux';
-import openai from '../utils/openai';
-import ErrorHandling from "./ErrorHandling";
 import { addGPTMovieResults } from '../utils/gptSlice';
 import { API_OPTIONS } from '../utils/constants';
-import MovieList from './MovieList';
 
 const GptSearchBar = () => {
   const dispatch = useDispatch();
@@ -21,7 +18,7 @@ const GptSearchBar = () => {
     // console.log('data >>', data);
     const json = await data.json();
 
-    console.log('json >>', json);
+    // console.log('json >>', json);
     return json.results;
   };//searchMovieTMDB
 
@@ -66,7 +63,7 @@ console.log(searchText.current.value );
   return (
     <div className="pt-[35%] md:pt-[10%] flex justify-center">
         <form className="w-full md:w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
-          <input ref={searchText} type="text" className="p-3 m-4 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder}/>
+          <input data-testid="languages" ref={searchText} type="text" className="p-3 m-4 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder}/>
           <button className="col-span-3 m-4 py-2 px-1 bg-red-700 text-white rounded-lg"
             onClick={handleGptSearchClick}
           >
---
+++ src/components/__tests__/GptSearchBar.test.js
@@ -0,0 +1,23 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import appStore from "../../utils/appStore";
+import "@testing-library/jest-dom";
+import GptSearchBar from '../GptSearchBar';
+
+it('Should show languages combobox on GPT search button', () => {
+    //Render the GptSearchBar Component
+    render(
+        <BrowserRouter>
+            <Provider store={appStore}>
+                <GptSearchBar/>
+            </Provider>
+        </BrowserRouter>
+    );
+    
+    // Use getAllByTestId to select all elements with a specific test ID
+  const languages = screen.getByPlaceholderText('What would you like to watch today?');
+
+  // Assertions
+  expect(languages).toBeInTheDocument();
+});
