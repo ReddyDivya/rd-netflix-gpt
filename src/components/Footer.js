f88e3a294a311f7e013744463f12b642ed71b1fb
Divya <divyareddy0205@gmail.com>
2024-01-10 13:40:58 -0800

    mobile responsive - footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,13 @@ import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section className="p-20 graybackgroundColor  text-gray-500 flex items-center justify-between">
-        <div className="grid grid-flow-row gap-3">
-            <div className="flex items-center gap-3 text-white">
+    /*
+    default - mobile, sm-tab, md-desktop
+    */
+    <section className="p-4 md:p-20 graybackgroundColor text-gray-500 flex md:flex-row items-center justify-between text-xs md:text-base md:gap-3">
+        <div className="grid grid-flow-row gap-2 md:gap-3 md:mr-16">
+            {/*first column*/}
+            <div className="flex items-center h-16 md:h-32 w-16 md:w-32 gap-2 md:gap-3 text-white">
                 <FaFacebook size={32}/>
                 <FaInstagram size={32}/>
                 <FaTwitter size={32} />
@@ -15,30 +19,32 @@ const Footer = () => {
             <a href="#investor-relations">Investor Relations</a>
             <a href="#legal-notice">Legal Notice</a>
             
-            <button className="w-32 h-18 border border-slate-400 py-2 px-4">Service Code</button>
+            <button className="w-16 md:w-32 h-18 md:h-18 border border-slate-400 py-2 px-4 mt-2 md:mt-0">Service Code</button>
 
-            <p>&copy; 1997-2024 Netflix, Inc.</p>
+            <p className="mt-4 md:mt-0">&copy; 1997-2024 Netflix, Inc.</p>
         </div>
+
         {/*second row*/}
-        <div className="grid grid-flow-row gap-3">
+        <div className="grid grid-flow-row gap-3 mx-auto ml-1 md:mr-16 mt-4 md:mt-0">
             <a href="#help-centre">Help Centre</a>
             <a href="#jobs">Jobs</a>
             <a href="#cookie-preferences">Cookie Preferences</a>
         </div>  
 
         {/*third row*/}
-        <div className="grid grid-flow-row gap-3">
+        <div className="grid grid-flow-row gap-3 md:mr-16 mt-4 md:mt-0">
             <a href="#gift-cards">Gift Cards</a>
             <a href="#terms-use">Terms of Use</a>
             <a href="#corporate-info">Corporate Information</a>
         </div>
 
         {/*fourth row*/}
-        <div className="grid grid-flow-row gap-3">
+        <div className="grid grid-flow-row gap-3 mt-4 md:mt-0">
             <a href="#media-centre">Media Centre</a>
             <a href="#privacy">Privacy</a>
             <a href="#contact-us">Contact Us</a>
         </div>
+    
     </section>
     
   )
