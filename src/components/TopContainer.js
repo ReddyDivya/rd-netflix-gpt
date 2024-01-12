fb7938e4e515af70b0556732f2fccb1292b9c976
Divya <divyareddy0205@gmail.com>
2024-01-12 01:29:21 -0800

    top container component

--- src/components/TopContainer.js
+++ src/components/TopContainer.js
@@ -54,18 +54,19 @@ const TopContainer = () => {
             src={IMG_CDN_URL + details.movieDetails?.backdrop_path}
           />
           <div className="w-full h-[250px] md:w-full md:h-screen absolute -z-10 bg-black md:bg-opacity-80 bg-opacity-60"></div>
-          <div className="md:w-full md:h-full md:flex md:flex-row flex flex-col">
-            
-            {/* Movie poster */}
-            <div className="w-28 relative top-16 md:top-0 left-10 md:left-0 md:w-4/12 md:p-20 flex md:pt-[130px]">
+          
+          <div className="md:w-full md:h-full md:flex md:flex-row flex flex-col items-center">            
+          
+            {/* left - Movie poster */}
+            <div className="w-28 relative top-16 md:top-0 md:bottom-6 left-10 md:left-0 md:w-4/12 md:p-20 flex md:pt-[130px]">
               <img
-                className="mx-auto rounded-lg"
+                className="mx-auto rounded-lg h-40 md:h-64"
                 alt="moviePoster"
                 src={IMG_CDN_URL + details.movieDetails?.poster_path}
               />
             </div>
 
-            <div className="w-full min-h-[500px] bg-[#0a0908] pt-5 md:bg-transparent relative top-20 md:top-0 flex flex-col items-center md:items-start text-center md:text-left md:w-8/12 md:pt-[140px] text-white">
+            <div className="w-full min-h-[500px] bg-[#0a0908] pt-5 md:bg-transparent relative top-20 md:top-20 flex flex-col md:flex md:flex-col justify-start items-start text-white">
               
               {/* Title */}
               <h1 className="px-4 md:px-0 text-3xl font-semibold ">
@@ -79,56 +80,56 @@ const TopContainer = () => {
 
                 <h3 className="mx-2">{hours + "h " + minutes + "m"}</h3>
               </div>
-            </div>
-            
-            {/* Genre*/}
-            <h3 className="flex">
-              {details.movieDetails?.genres.map((genre) => (
-                <Genre key={genre?.id} genre={genre?.name} />
-              ))}
-            </h3>
 
-            {/* Rating */}
-            <div className="flex md:my-3 py-2">
-              <div className="w-11 mx-1 px-1 py-2 md:px-0 md:w-12 md:py-3 md:mr-2">
-                <CircularRatingBar rating={details.movieDetails?.vote_average.toFixed(1)}
-                />
+              {/* Genre*/}
+              <h3 className="flex">
+                {details.movieDetails?.genres.map((genre) => (
+                  <Genre key={genre?.id} genre={genre?.name} />
+                ))}
+              </h3>
+              
+              {/* Rating */}
+              <div className="flex md:my-3 py-2">
+                <div className="w-11 mx-1 px-1 py-2 md:px-0 md:w-12 md:py-3 md:mr-2">
+                  <CircularRatingBar rating={details.movieDetails?.vote_average.toFixed(1)}
+                  />
+                </div>
+
+                {/* add to favourite */} 
+                <button
+                  onClick={addToFavourite}
+                  className="md:mx-4 mx-1 px-2 py-2 transition hover:-translate-y-1 after:text-red-600"
+                >
+                  <GiSelfLove size={32}/>
+                </button>
+                
+                {/* save to watchlist */}
+                <button
+                  onClick={addToWatchList}
+                  className="md:mx-4 mx-1 px-2 py-2  transition hover:-translate-y-1 after:text-red-600"
+                >
+                  <MdOutlineBookmarkAdd size={32}/>
+                </button>
+                
+
+                <button className="md:mx-4 mx-1 px-2 py-2  transition hover:-translate-y-1 after:text-red-600">
+                  <MdOutlineBookmarkAdd size={32}/>
+                </button>
+                <button
+                  onClick={() => {
+                    handlePlay();
+                  }}
+                  className="text-xl mx-1 px-2 py-2  transition hover:-translate-y-1 nd:px-2 text-white rounded-lg"
+                >
+                  <FaPlayCircle size={32}/> Play
+                </button>
               </div>
 
-              {/* add to favourite */} 
-              <button
-                onClick={addToFavourite}
-                className="md:mx-4 mx-1 px-2 py-2 transition hover:-translate-y-1 after:text-red-600"
-              >
-                <GiSelfLove size={32}/>
-              </button>
-              
-              {/* save to watchlist */}
-              <button
-                onClick={addToWatchList}
-                className="md:mx-4 mx-1 px-2 py-2  transition hover:-translate-y-1 after:text-red-600"
-              >
-                <MdOutlineBookmarkAdd size={32}/>
-              </button>
-              
-
-              <button className="md:mx-4 mx-1 px-2 py-2  transition hover:-translate-y-1 after:text-red-600">
-                <MdOutlineBookmarkAdd size={32}/>
-              </button>
-              <button
-                onClick={() => {
-                  handlePlay();
-                }}
-                className="text-xl mx-1 px-2 py-2  transition hover:-translate-y-1 nd:px-2 text-white rounded-lg"
-              >
-                <FaPlayCircle size={32}/> Play
-              </button>
-            </div>
-            
-            {/* overview */}
-            <div className="px-4 py-2 pb-5 md:px-0">
-              <h3 className="text-xl font-medium ">Overview</h3>
-              <p className="md:w-3/4">{details.movieDetails?.overview}</p>
+              {/* overview */}
+              <div className="px-4 py-2 pb-5 md:px-0">
+                <h3 className="text-xl font-medium">Overview</h3>
+                <p className="md:w-3/4">{details.movieDetails?.overview}</p>
+              </div>
             </div>
           </div>
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,7 +20,7 @@ export const API_OPTIONS = {
 };
 
 // export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
-
+export const OPENAI_KEY = "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"
 
 export const SUPPORTED_LANGUAGES = [
   {identifier : "en", name: "English"},
